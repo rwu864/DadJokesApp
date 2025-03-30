21a4164e561eee745a9134e0a25936e27c2b9af8
rwu864 <raywu864@gmail.com>
2025-03-30 14:31:09 -0700

    ui - search joke section

---
+++ DadJokesApp/DadJokesApp.Web/Pages/SearchJoke.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DadJokesApp.Web.Pages
+{
+    public class SearchJokeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
