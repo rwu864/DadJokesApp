0459c137c871b7845683c930c264499ff85c0176
rwu864 <raywu864@gmail.com>
2025-03-29 21:07:55 -0700

    api - search unit tests for jokescontroller

--- DadJokesApp/DadJokesApp.Api.Tests/ControllerTests/JokesControllerTests.cs
+++ DadJokesApp/DadJokesApp.Api.Tests/ControllerTests/JokesControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture;
 using DadJokesApp.Api.Common;
 using DadJokesApp.Api.Controllers;
+using DadJokesApp.Api.Models;
 using DadJokesApp.Api.Services;
 using FakeItEasy;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ public class JokesControllerTests
     {
         _fixture = new Fixture();
     }
+
+    #region GetRandomJoke
 
     [Fact]
     public async Task GetRandomJoke_ReturnsOk_WhenJokeServiceSucceeds()
@@ -77,4 +80,107 @@ public class JokesControllerTests
         var problemDetails = Assert.IsType<ProblemDetails>(problemResult.Value);
         Assert.Equal("Unable to fetch a joke at this time.", problemDetails.Detail);
     }
+
+    #endregion GetRandomJoke
+
+    #region SearchJokes
+
+    [Fact]
+    public async Task SearchJokes_ReturnsBadRequest_WhenTermIsEmpty()
+    {
+        // Arrange
+        var fakeService = A.Fake<IJokeService>();
+        var controller = new JokesController(fakeService);
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+        // Act
+        var result = await controller.SearchJokes(string.Empty);
+
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal(400, badRequestResult.StatusCode);
+        var problemDetails = Assert.IsType<ProblemDetails>(badRequestResult.Value);
+        Assert.Equal("Invalid search parameter", problemDetails.Title);
+        Assert.Equal("Search term cannot be empty or whitespace", problemDetails.Detail);
+    }
+
+    [Fact]
+    public async Task SearchJokes_ReturnsBadRequest_WhenTermIsWhitespace()
+    {
+        // Arrange
+        var fakeService = A.Fake<IJokeService>();
+        var controller = new JokesController(fakeService);
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+        // Act
+        var result = await controller.SearchJokes(string.Empty);
+
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal(400, badRequestResult.StatusCode);
+        var problemDetails = Assert.IsType<ProblemDetails>(badRequestResult.Value);
+        Assert.Equal("Invalid search parameter", problemDetails.Title);
+        Assert.Equal("Search term cannot be empty or whitespace", problemDetails.Detail);
+    }
+
+    [Fact]
+    public async Task SearchJokes_ReturnsOk_WhenJokeServiceSucceeds()
+    {
+        // Arrange
+        var fakeService = A.Fake<IJokeService>();
+        var expectedResult = _fixture.Create<JokeSearchModel>();
+        A.CallTo(() => fakeService.SearchJokesAsync(A<string>._))
+            .Returns(ServiceResult<JokeSearchModel>.Ok(expectedResult));
+        var controller = new JokesController(fakeService);
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+        // Act
+        var result = await controller.SearchJokes(_fixture.Create<string>());
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(200, okResult.StatusCode);
+        Assert.Equal(expectedResult, okResult.Value);
+    }
+
+    [Fact]
+    public async Task SearchJokes_CallsServiceOnce_WhenInvoked()
+    {
+        // Arrange
+        var fakeService = A.Fake<IJokeService>();
+        var searchTerm = _fixture.Create<string>();
+        A.CallTo(() => fakeService.SearchJokesAsync(A<string>._))
+            .Returns(ServiceResult<JokeSearchModel>.Ok(_fixture.Create<JokeSearchModel>()));
+        var controller = new JokesController(fakeService);
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+        // Act
+        await controller.SearchJokes(searchTerm);
+
+        // Assert
+        A.CallTo(() => fakeService.SearchJokesAsync(searchTerm))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task SearchJokes_ReturnsProblem_WhenJokeServiceFails()
+    {
+        // Arrange
+        var fakeService = A.Fake<IJokeService>();
+        A.CallTo(() => fakeService.SearchJokesAsync(A<string>._))
+            .Returns(ServiceResult<JokeSearchModel>.Fail());
+        var controller = new JokesController(fakeService);
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+        // Act
+        var result = await controller.SearchJokes(_fixture.Create<string>());
+
+        // Assert
+        var problemResult = Assert.IsType<ObjectResult>(result.Result);
+        Assert.Equal(500, problemResult.StatusCode);
+        var problemDetails = Assert.IsType<ProblemDetails>(problemResult.Value);
+        Assert.Equal("Unable to fetch a joke at this time.", problemDetails.Detail);
+    }
+
+    #endregion
 }
